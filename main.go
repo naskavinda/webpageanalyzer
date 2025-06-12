a82c7c98076e4c7de65392e6638c2b2c606cfb49
Supun Kavinda <naskavinda@gmail.com>
2025-06-12 22:19:00 +0530

    fixed issue in handler class calling

--- main.go
+++ main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/naskavinda/webpageanalyzer/analyzer"
 	. "github.com/naskavinda/webpageanalyzer/handlers"
 )
 
 func main() {
 	fmt.Println("Welcome to the Web Page Analyzer!")
 	r := gin.Default()
-	w := WebPageAnalyzer{}
+	w := WebPageAnalyzer{
+		Service: analyzer.DefaultAnalyzerService{},
+	}
 	r.POST("/analyzer", w.WebPageAnalyzerHandler)
 
 	r.Run()

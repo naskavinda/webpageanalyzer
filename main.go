68f71044320399f9b77c82d67f8bbca7da04cf1a
Supun Kavinda <naskavinda@gmail.com>
2025-06-13 02:31:03 +0530

    Fixed CORS issue

--- main.go
+++ main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/naskavinda/webpageanalyzer/analyzer"
 	. "github.com/naskavinda/webpageanalyzer/handlers"
+	"time"
 )
 
 func main() {
 	fmt.Println("Welcome to the Web Page Analyzer!")
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	w := WebPageAnalyzer{
 		Service: analyzer.DefaultAnalyzerService{},
 	}
